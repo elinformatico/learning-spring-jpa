576c0814e2c8a5b4fcd0cd24fbeaa07434fc01a7
elinformatico <donoe1985@gmail.com>
2021-06-11 19:42:02 -0500

    chore: Creacion de Proyecto Inicial

---
+++ src/test/java/net/elinformatico/springjpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.elinformatico.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
