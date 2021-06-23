11c9e745543dbc041325457504a4732afb27d094
elinformatico <donoe1985@gmail.com>
2021-06-22 19:53:45 -0500

    chore: Configurando Entidad Perfiles para JPA

---
+++ src/main/java/net/elinformatico/service/IPerfilesService.java
@@ -0,0 +1,21 @@
+package net.elinformatico.service;
+
+import java.util.List;
+
+import net.elinformatico.model.Perfil;
+import net.elinformatico.model.Vacante;
+import net.elinformatico.repository.PefilesRepository;
+
+public interface IPerfilesService {
+	public PefilesRepository obtenerRepo();
+	public Perfil guardar(Perfil perfil);
+	public Perfil buscarPorId(Integer id);
+	public boolean existePorId(Integer id);
+	public void eliminarPorId(Integer id);
+	public void eliminarPorEntidad(Perfil perfil);
+	public long numeroRegistros();
+	public void borrarTodo();
+	public List<Perfil> buscarTodos();
+	public List<Perfil> obtenerUsuario(List<Integer> ids);
+	public List<Perfil> guardarVarios(Iterable<Vacante> usuarios);
+}
---
+++ src/main/java/net/elinformatico/service/jpa/PerfilesService.java
@@ -0,0 +1,82 @@
+package net.elinformatico.service.jpa;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import net.elinformatico.model.Perfil;
+import net.elinformatico.model.Vacante;
+import net.elinformatico.repository.PefilesRepository;
+import net.elinformatico.service.IPerfilesService;
+
+@Service
+public class PerfilesService implements IPerfilesService {
+
+	private PefilesRepository repo;
+	
+	@Override
+	public PefilesRepository obtenerRepo() {
+		return repo;
+	}
+
+	@Override
+	public Perfil guardar(Perfil perfil) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Perfil buscarPorId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existePorId(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void eliminarPorId(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void eliminarPorEntidad(Perfil perfil) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long numeroRegistros() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void borrarTodo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Perfil> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Perfil> obtenerUsuario(List<Integer> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Perfil> guardarVarios(Iterable<Vacante> usuarios) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
